e3b45ec069aabc3c011735cf70aae83062124749
Sadesh Warnakulasooriya <>
2020-09-27 16:47:31 +0900

    Initial Setup of the Application

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+        
+        {/* Spotify Logo */}
+            
+         {/* Login with Spotify button */}
+        </div>
+    )
+}
+
+export default Login
+{/*  */}
